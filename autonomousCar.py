a0070f7b8471be957303a7706c44e8d148bcc469
AlperenCicek <alperencicek1998@gmail.com>
2020-01-06 04:49:07 +0300

    06.01.2020-4:49UpdateCommit

--- autonomousCar.py
+++ autonomousCar.py
@@ -4,6 +4,6 @@ from videoProcessing import videoProcessing
 imageName = "RoadPhotoWithSign.png"
 videoName = "RoadVideo_Trim1.mp4"
 imageProcessing = imageProcessing(imageName)
-imageProcessing.displayingScreen(imageName, imageProcessing.imageProcessingFunction())
+imageProcessing.displayingScreen(imageName, imageProcessing.imageProcess())
 videoProcessing = videoProcessing(videoName)
-videoProcessing.videoProcessingFunction()
+videoProcessing.videoProcess()
