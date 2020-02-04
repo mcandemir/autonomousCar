89bded078d5f22bba7ae924603372e444097fb6e
Alperen Çiçek <alperencicek1998@gmail.com>
2020-02-04 17:07:10 +0300

    uploadingMediaCommit

---
+++ New folder/asd.py
@@ -0,0 +1,5 @@
+from imageProcessing import imageProcessing
+
+imageName = "deneme4.png"
+imageProcessing = imageProcessing(imageName)
+imageProcessing.displayingScreen(imageName, imageProcessing.imageProcess())
