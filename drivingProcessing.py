6d164072064d5a6462b6e37b67ef4d7202830b32
Alperen Çiçek <alperencicek1998@gmail.com>
2020-03-03 18:23:28 +0300

    03.03.2020UpdateCommit_2

--- drivingProcessing.py
+++ drivingProcessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from mainCamera import MainCamera
 from sideCamera import SideCamera
+from motorProcessing import MotorProcessing
 import time
 
 class DrivingProcessing:
@@ -18,6 +19,7 @@ class DrivingProcessing:
         cap = cv2.VideoCapture(1)
         cap.set(3, self.w)
         cap.set(4, self.h)
+        motor = MotorProcessing(15, 22)
         while(True):
             _, frame = cap.read()
             if self.camera == "MainCamera":
@@ -31,15 +33,22 @@ class DrivingProcessing:
                 break
             if self.error is not True:
                 if self.left == True:
+                    motor.motorFunc(False, True)
                     print("TURN LEFT.")
                 elif self.right == True:
+                    motor.motorFunc(True, False)
                     print("TURN RIGHT.")
                 elif self.forward == True:
+                    motor.motorFunc(False, False)
                     print("OK. Slope:")
             else:
                 print("No lane detected.")
+                motor.motorFunc(False, False)
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                motor.motorFunc(False, False)
                 break
 
         cap.release()
         cv2.destroyAllWindows()
+
+
